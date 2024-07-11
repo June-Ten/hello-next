50da5184fd3daf5954464c509de71958bc492057
wangqiling <891696668@qq.com>
2024-07-11 19:15:26 +0800

    测试github webhooks jenkins

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      Press me
+      Press me 测试github webhooks jenkins
     </main>
   );
 }
